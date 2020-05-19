976d86207d406c570c067ecbb8eef62ca5aef57b
SUNG IL LEE <sungil.lee90@gmail.com>
2020-05-19 15:08:02 -0500

    Complete warmup exercise for jUnit.

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -14,28 +14,38 @@ public class MyFirstTest {
         String company = "Codeup";
         String testing = "Codeup";
 
-        assertEquals(company, testing);
+        assertEquals(company, testing); // to compare the values (individual values)
     }
 
     // todo Create a test method that uses assertNotSame
     //  to verify that the following ArrayLists are not the same:
     @Test
-    public void mySecondTest() {
+    public void testIfListsAreNotTheSame() {
         List<String> languages = new ArrayList<>();
         List<String> moreLanguages = new ArrayList<>();
 
-        assertNotSame(languages, moreLanguages);
+        assertNotSame(languages, moreLanguages); // SAME for the Objects (LIST, hashMap, etc)
     }
 
     // todo Create a test method that uses assertArrayEquals to verify the following arrays are equal.
-//    @Test
-//    public void myThirdTest() {
-//        int[] numbers = {1, 2, 3};
-//        int[] otherNumbers = new int[3];
-//        otherNumbers[0] = 1;
-//        otherNumbers[1] = 2;
-//        otherNumbers[2] = 3;
-//
-//        assertArrayEquals
-//    }
+    @Test
+    public void testIfArraysAreEqual() {
+        int[] numbers = {1, 2, 3};
+        int[] otherNumbers = new int[3];
+        otherNumbers[0] = 1;
+        otherNumbers[1] = 2;
+        otherNumbers[2] = 3;
+
+        assertArrayEquals(numbers, otherNumbers);
+    }
+
+    // todo Create a test method that uses both assertFalse and assertTrue
+    //  to verify the following statements:
+    @Test
+    public void testIfWordContains() {
+        String language = "PHP";
+        assertTrue(language.contains("H")); // use assertTrue for this statement
+        assertFalse(language.contains("J")); // use assertFalse for this statement
+    }
+
 }
